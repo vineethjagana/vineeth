b3ddd486833505ff69024efe7b4c337ab8c0d0bf
9492848417 <53636977+9492848417@users.noreply.github.com>
2020-07-27 10:28:08 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+
+# Path of working folder on Disk
+
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite("removed_noise.png", img)
+
+    #  Apply threshold to get image with only black and white
+    
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite(img_path, img)
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(img_path))
+
+    # Remove template file
+    
+
+    return result
+
+
+print ("_____The Recognization Process Of Text From The Input Image Started Successfully______")
+print ("The Recognized Text From The Input Image IS:")
+print (get_string(filename))
+
+print ("_____Process Finished_______")
